72282f670f1361204065cf68acec04179306b3b4
Lomachenko84 <agent84@inbox.ru>
2023-07-23 21:12:41 +0300

    ticket1.1

--- http/helpdesk/frontend/src/js/HelpDesk.js
+++ http/helpdesk/frontend/src/js/HelpDesk.js
@@ -9,6 +9,24 @@ export default class HelpDesk {
       throw new Error("This is not HTML element!");
     }
     this.container = container;
+    console.log(this.container)
     this.ticketService = new TicketService();
+
   }
+
+  init() {
+    console.log("init")
+    this.ticketService.list((responce) => {
+      console.log(responce)
+      this.tickets = responce;
+    })
+    this.renderTiketList();
+  }
+
+renderTiketList() {
+  let list = document.createElement("div")
+  list.className = "list"
+  th
+
+}
 }
--- http/helpdesk/frontend/src/js/api/createRequest.js
+++ http/helpdesk/frontend/src/js/api/createRequest.js
@@ -1,4 +1,18 @@
 const createRequest = async (options = {}) => {
+    let queryString = options.params
+    ? Object.keys(options.params).map(key => `${key}=${options.params[key]}`).join('&')
+    : ""
+    // queryString = "id=1242424&idjhghdg=dsfsdfgs"
+
+    let URL = `http://localhost:3000?method=${options.apiMethod}${queryString ? `&${queryString}`:""}`;
+   
+    // URL = http://localhost:3000?method=allTickets&id=1242424&idjhghdg=dsfsdfgs
+
+    let responce = await fetch(URL, {
+        method: options.method ? options.method : "GET",
+        body: options.body ? JSON.stringify(options.body) : undefined
+    });
+    return responce.json();
 };
 
 export default createRequest;
